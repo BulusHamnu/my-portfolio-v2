120f73cfb38f9b79d27e8cc31ff00d57e19a80d3
BulusHamnu <hamnubulus@gmail.com>
2026-05-01 09:06:42 +0100

    Feat: added header and minimal hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-/* App entry */
+import Header from "./components/header";
+
+/* Home */
 function App() {
   return (
     <div className="container">
-      <h1>Bulus Hamnu - Backend Developer</h1>
-      <p className="text-left">
-        Hi, I'm Bulus Hamnu, a passionate web developer skilled in creating
-        intuitive, user-friendly websites and applications. With expertise in
-        HTML, CSS, JavaScript, and modern frameworks, I focus on delivering
-        responsive and seamless user experiences. Feel free to reach out if
-        you'd like to discuss potential collaborations or opportunities.
-      </p>
+      <Header />
+      <section className="hero-section pt-28">
+        <span
+          style={{ fontSize: "12px" }}
+          className="border-b-gray-700 px-4 py-1.5 rounded-2xl bg-gray-900 text-shadow-sm m-3"
+        >
+          Backend Developer
+        </span>
+        <h1>
+          Hi, I'm <span className="text-amber-500">Hamnu Bulus</span> <br />
+          Madu
+        </h1>
+        <p className="text-left">
+          A passionate software developer with a strong interest in backend and
+          full-stack development. I have hands-on experience with HTML, CSS,
+          JavaScript, React.js, Python, and Node.js. If you’d like to connect or
+          discuss potential collaborations and opportunities, feel free to reach
+          out!
+        </p>
+      </section>
     </div>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { X } from "lucide-react";
+
+/* Header */
+function Logo() {
+  return (
+    <div className="font-bold text-2xl">
+      <span className="logo">Bulus</span>
+    </div>
+  );
+}
+
+function Menu({
+  isOpen,
+  toogleMenu,
+}: {
+  isOpen: boolean;
+  toogleMenu: () => void;
+}) {
+  return (
+    <div
+      onClick={toogleMenu}
+      className="rounded nav-menu  cursor-pointer  p-1.5"
+    >
+      {isOpen ? (
+        <X size={30} />
+      ) : (
+        <div className="flex flex-col justify-items-start items-end">
+          <span className="bg-amber-50 p-0.5 mb-1 w-7 rounded"></span>
+          <span className="bg-amber-50 p-0.5 mb-1 w-5 rounded"></span>
+          <span className="bg-amber-50 p-0.5 mb-1 w-6 rounded"></span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function LinkItem({ text }: { text: string }) {
+  return (
+    <li className="nav-link-item cursor-pointer px-2 py-3 text-left">{text}</li>
+  );
+}
+
+function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toogleMenu() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <header className="h-15 flex flex-row justify-between items-center fixed top-0 w-full px-3 z-20">
+      <Logo />
+      <Menu isOpen={isOpen} toogleMenu={toogleMenu} />
+      <nav className={`main-navbar  ${isOpen ? "active" : ""}`}>
+        <ul className="flex flex-col w-full justify-center">
+          <LinkItem text="Home" />
+          <LinkItem text="Projects" />
+          <LinkItem text="Skills" />
+          <LinkItem text="About Me" />
+          <LinkItem text="Contact" />
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
