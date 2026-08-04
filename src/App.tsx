da61821dcf2eabb0a347a7448d3290ee0ad26e72
BulusHamnu <hamnubulus@gmail.com>
2026-08-04 10:07:41 +0100

    feat: add scrollToTop component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Music from "./pages/music";
+import ScrollToTop from "./components/scrollToTop";
 
 /* Home */
 function App() {
   return (
     <div className="container">
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/music" element={<Music />} />
